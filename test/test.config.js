4728aeb01a785dbc3432bcc7415b8de098e0d2e8
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-02-06 18:55:24 -0500

    Initial commit.

---
+++ test/test.config.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {config} = require('bedrock');
+const path = require('path');
+
+config.karma.suites['bedrock-web-vc-store] = path.join('web', '**', '*.js');
---
+++ test/test.js
@@ -0,0 +1,9 @@
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+
+const bedrock = require('bedrock');
+require('bedrock-karma');
+
+require('bedrock-test');
+bedrock.start();

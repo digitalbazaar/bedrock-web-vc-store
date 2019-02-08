a67ab8db673bed630c21e23fc55da7de1bc3143f
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-02-08 15:56:40 -0500

    Add passing tests for insert, get, delete.

--- test/web/10-VerifiableCredentialStore.test.js
+++ test/web/10-VerifiableCredentialStore.test.js
@@ -1,11 +1,55 @@
 /*!
  * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
  */
-// import {store, MemoryEngine} from 'bedrock-web-store';
 import VerifiableCredentialStore from 'bedrock-web-vc-store';
 
+import {store, MemoryEngine} from 'bedrock-web-store';
+import {getRemoteStorage} from 'bedrock-web-data-hub';
+import {mock} from './mock.js';
+
+const password = 'password';
+
+let vcStore;
+
 describe('VerifiableCredentialStore', () => {
-  it('should exist', () => {
-    should.exist(VerifiableCredentialStore);
-  })
+  before(async () => {
+    store.setEngine({engine: new MemoryEngine()});
+    mock.init();
+    const remoteStorage = await getRemoteStorage({accountId: 'test'});
+    await remoteStorage.createMasterKey({password});
+
+    vcStore = new VerifiableCredentialStore({remoteStorage});
+  });
+
+  it('should insert a credential', async () => {
+    const credential = {id: 'foo', someKey: 'someValue'};
+    await vcStore.insert({credential});
+  });
+
+  it('should get a credential', async () => {
+    const expected = {id: 'foo', someKey: 'someValue'};
+    const vc = await vcStore.get({id: expected.id});
+    vc.should.deep.equal(expected);
+  });
+
+  it('should delete an existing credential', async () => {
+    const result = await vcStore.delete({id: 'foo'});
+    result.should.equal(true);
+  });
+
+  it('should fail to get a deleted credential', async () => {
+    let err;
+    try {
+      await vcStore.get({id: 'foo'});
+    } catch(e) {
+      err = e;
+    }
+    should.exist(err);
+    err.name.should.equal('NotFoundError');
+  });
+
+  it('should fail to delete a non-existent credential', async () => {
+    const result = await vcStore.delete({id: 'foo'});
+    result.should.equal(false);
+  });
 });

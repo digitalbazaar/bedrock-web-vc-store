8acc23870cc2e5605d63af3f215f89946ed90f58
Matthew Collier <mcollier@digitalbazaar.com>
2020-06-29 10:36:26 -0400

    Remove strict mode for ES6.

--- test/web/mock.js
+++ test/web/mock.js
@@ -1,6 +1,7 @@
 /*!
  * Copyright (c) 2018-2020 Digital Bazaar, Inc. All rights reserved.
  */
+
 import Pretender from 'pretender';
 import {EdvClient} from 'edv-client';
 import {MockStorage} from 'bedrock-web-mock-data-hub-storage';

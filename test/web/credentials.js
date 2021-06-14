b3eea01aa85709a33abd5ddefe78bb3acd0589c1
Tashi D. Gyeltshen <38434620+JSAssassin@users.noreply.github.com>
2021-06-14 16:29:15 -0400

    Fix tests (#11)
    * Setup and fix tests.
    
    * Skip failing test.
    
    * Remove devDependencies in test package.json and engines.
    
    * Rebase on fix-query and remove mock invoker and kak.
    
    * Remove unused deps and logs, and update mock credential.
    
    * Fix array for type test.
    
    Co-authored-by: Josh Dunn <jdunn@digitalbazaar.com>

--- test/web/credentials.js
+++ test/web/credentials.js
@@ -10,12 +10,16 @@ const AlumniCredential = {
   // such as 'issuer' and 'alumniOf'.
   "@context": [
     "https://www.w3.org/2018/credentials/v1",
-    "https://www.w3.org/2018/credentials/examples/v1"
+    "https://www.w3.org/2018/credentials/examples/v1",
+    "https://w3id.org/security/ed25519-signature-2020/v1"
   ],
   // specify the identifier for the credential
   "id": "http://example.edu/credentials/1872",
   // the credential types, which declare what data to expect in the credential
-  "type": ["VerifiableCredential", "AlumniCredential"],
+  "type": [
+    "VerifiableCredential",
+    "AlumniCredential"
+  ],
   // the entity that issued the credential
   "issuer": "https://example.edu/issuers/565049",
   // when the credential was issued
@@ -30,16 +34,12 @@ const AlumniCredential = {
   // digital proof that makes the credential tamper-evident
   "proof": {
     // the cryptographic signature suite that was used to generate the signature
-    "type": "RsaSignature2018",
+    "type": "Ed25519Signature2020",
     // the date the signature was created
-    "created": "2017-06-18T21:19:10Z",
-    // the public key identifier that created the signature
-    "creator": "https://example.edu/issuers/keys/1",
-    // the digital signature value
-    "jws": "eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..TCYt5X" +
-      "sITJX1CxPCT8yAV-TVkIEq_PbChOMqsLfRoPsnsgw5WEuts01mq-pQy7UJiN5mgRxD-WUc" +
-      "X16dUEMGlv50aqzpqh4Qktb3rk-BuQy72IFLOqV0G_zS245-kronKb78cPN25DGlcTwLtj" +
-      "PAYuNzVBAh4vGHSrQyHUdBBPM"
+    "created": "2020-05-18T21:19:10Z",
+    "proofPurpose": "assertionMethod",
+    "proofValue": "z3vG9cHevmrtMiTfb8e7qSPtKyZz1ziPbcxePqcYJ5Rtx5asWsHFq6rPfj8GaPxXkYqvb7qu2dFYg9amc1dpqQhsY",
+    "verificationMethod": "https://example.edu/issuers/565049#z6MkjLrk3gKS2nnkeWcmcxiZPGskmesDpuwRBorgHxUXfxnG"
   }
 };
 
---
+++ test/web/mockServer.js
@@ -0,0 +1,124 @@
+/*!
+ * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
+ */
+import {httpClient} from '@digitalbazaar/http-client';
+import sinon from 'sinon';
+import pathToRegexp from 'path-to-regexp';
+import routeParams from 'route-params';
+
+/**
+ * This is the sinon stub mock server.
+ * It lacks some of the functionality of Pretender
+ * however it can run alot of tests in node using mocha in less time.
+ *
+ * @class MockServer
+ */
+export class MockServer {
+  constructor() {
+    this.sandbox = sinon.createSandbox();
+    this.stubs = new Map();
+    this.removeStubs();
+    this.stubs.set('post', this.sandbox.stub(httpClient, 'post'));
+    this.stubs.set('get', this.sandbox.stub(httpClient, 'get'));
+    this.stubs.set('delete', this.sandbox.stub(httpClient, 'delete'));
+    this.post = this.route(this.stubs.get('post'));
+    this.get = this.route(this.stubs.get('get'));
+    this.delete = this.route(this.stubs.get('delete'));
+  }
+  // this loops through the various HTTP methods
+  // and removes any stubs. it makes mocha --watch possible
+  removeStubs() {
+    const methods = [
+      'post', 'get', 'delete', 'put', 'options', 'head', 'patch'];
+    methods.forEach(method => {
+      if(httpClient[method] && httpClient[method].restore) {
+        httpClient[method].restore();
+      }
+    });
+  }
+  /**
+   * @description This is the core of the sinon mock server.
+   * It takes in a stub which is then setup to a regex
+   * to match a route and an async function that will handle the mock data.
+   *
+   * @param {sinon.stub} stub - A sinon stub.
+   *
+   * @returns {Function} A function that will allow other services
+   * should as mock storage and mock kms to setup test data.
+   */
+  route(stub) {
+    /**
+     * This is a function curried to a stub.
+     *
+     * @param {string} path - A valid express route path ex: edvs/:id.
+     * @param {Function} callback - A function that accepts the route params
+     * and then produces mock data for a test.
+     *
+     * @returns {object} - The result of the callback.
+     */
+    return function(path, callback) {
+      const pathRegex = pathToRegexp(path);
+      return stub
+        .withArgs(sinon.match(value => pathRegex.test(value)))
+        .callsFake(async function(route, body) {
+
+          const params = routeParams(path, route);
+          const queryParams = (body && body.searchParams) ?
+            body.searchParams : {};
+          const {headers} = body ? body : {headers: {}};
+          for(const [key, value] of Object.entries(queryParams)) {
+            queryParams[key] = String(value);
+          }
+          const request = {
+            route,
+            requestBody: JSON.stringify(body),
+            headers,
+            params,
+            queryParams
+          };
+          const result = await callback(request);
+          // the first argument from a handler is the statusCode in express.
+          const [status] = result;
+          if(status > 300) {
+            const error = new Error('A HTTP error occurred.');
+            error.response = {
+              headers: new Map([['content-type', 'application/json']]),
+              json: async () => ({}),
+              status,
+            };
+            switch(status) {
+              case 404:
+                error.name = 'NotFoundError';
+                throw error;
+              case 409:
+                error.name = 'DuplicateError';
+                throw error;
+              default:
+                throw error;
+            }
+          }
+
+          let [, responseHeaders] = result;
+          responseHeaders = responseHeaders || new Map();
+          responseHeaders.set('content-type', 'application/json');
+          // this might look weird, but express really does
+          // reserve the last argument from a handler for the data.
+          // this formats that data into a http response
+          return {
+            headers: responseHeaders,
+            json: async () => result[result.length - 1],
+            status,
+          };
+        });
+    };
+  }
+  shutdown() {
+    return true;
+  }
+  prepareHeaders() {
+    return true;
+  }
+  prepareBody() {
+    return true;
+  }
+}

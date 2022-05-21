9c757ef29556c48cc40d9e8160ab8595e391874c
Dave Longley <dlongley@digitalbazaar.com>
2022-05-20 20:05:39 -0400

    Update test deps.

--- test/test.config.js
+++ test/test.config.js
@@ -2,7 +2,8 @@
  * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
-import path from 'path';
+import path from 'node:path';
+import webpack from 'webpack';
 import '@bedrock/karma';
 
 config.karma.suites['bedrock-web-vc-store'] = path.join('web', '**', '*.js');
@@ -10,3 +11,9 @@ config.karma.suites['bedrock-web-vc-store'] = path.join('web', '**', '*.js');
 // disable babel preprocessing during karma tests to avoid
 // errors with dated bedrock-karma and features like spread operator
 config.karma.defaults.DEFAULT_PREPROCESSORS = ['webpack'];
+
+config.karma.config.webpack.plugins.push(
+  // pathname-match > assert > util > process
+  new webpack.ProvidePlugin({
+    process: 'process'
+  }));
